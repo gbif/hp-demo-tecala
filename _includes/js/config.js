64703a59ea34e72e5423f4e568cfc30eabac01f9
Morten Høfft <mhoefft@gbif.org>
2021-04-26 14:47:00 +0200

    scope occurrences to specimen data from collections

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -1,7 +1,36 @@
-var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', extendWith: {
-  primary: themeStyle.colors.primary
-}});
+var siteTheme = gbifReactComponents.themeBuilder.extend({
+  baseTheme: 'light', extendWith: {
+    primary: themeStyle.colors.primary
+  }
+});
 
 var siteConfig = {
-  rootPredicate: { "type": "equals", "key": "taxonKey", "value": "5"}
+  rootPredicate: {
+    "type": "and",
+    "predicates": [
+      {
+        "type": "or",
+        "predicates": [
+          {
+            "type": "isNotNull",
+            "key": "institutionKey"
+          },
+          {
+            "type": "isNotNull",
+            "key": "collectionKey"
+          }
+        ]
+      },
+      {
+        "type": "in",
+        "key": "basisOfRecord",
+        "values": [
+          "PRESERVED_SPECIMEN",
+          "FOSSIL_SPECIMEN",
+          "MATERIAL_SAMPLE",
+          "LIVING_SPECIMEN"
+        ]
+      }
+    ]
+  }
 };
